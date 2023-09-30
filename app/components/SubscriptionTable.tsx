fb1aecf284f1d4fa4725d963471b4da2767056d4
Outkarm <jkpordje@gmail.com>
2023-09-30 07:59:14 +0000

    add switch style name for the status

--- app/components/SubscriptionTable.tsx
+++ app/components/SubscriptionTable.tsx
@@ -1,9 +1,32 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Props } from "../interfaces/Props";
 import "../styles/subscription.css";
 
 const SubscriptionsTable: FC<Props> = (props) => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
   const { subscriptions } = props;
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  };
+
+  function getStatusStyle(status) {
+    switch (status) {
+      case "Active":
+        return "active";
+      case "Expired":
+        return "expired";
+      case "Cancelled":
+        return "cancelled";
+      case "Pending":
+        return "pending";
+      case "Blocked":
+        return "blocked";
+      default:
+        return "default";
+    }
+  }
 
   return (
     <div className="subscription-table">
@@ -30,11 +53,7 @@ const SubscriptionsTable: FC<Props> = (props) => {
               <td>{subscription.subscriptionName}</td>
               <td>{subscription.paymentCycle}</td>
               <td>
-                <span
-                  className={
-                    subscription.status === "Active" ? "active" : "inactive"
-                  }
-                >
+                <span className={getStatusStyle(subscription.status)}>
                   {subscription.status}
                 </span>
               </td>
@@ -48,15 +67,22 @@ const SubscriptionsTable: FC<Props> = (props) => {
                 <a className="renew-button">Renew</a> |{" "}
                 <a className="auto-renew-button">Enable Auto Renew</a> |{" "}
                 <a className="upgrade-button">Upgrade</a> |{" "}
-                <a className="more-button">
-                  More{" "}
-                  <img
-                    width="15"
-                    height="15"
-                    src="https://img.icons8.com/material-rounded/10/sort-down.png"
-                    alt="sort-down"
-                  />
-                </a>
+                <div className="more-button-container">
+                  <button className="more-button" onClick={toggleDropdown}>
+                    More{" "}
+                    <img
+                      width="15"
+                      height="15"
+                      src="https://img.icons8.com/material-rounded/10/sort-down.png"
+                      alt="sort-down"
+                    />
+                  </button>
+                  {isDropdownVisible && (
+                    <div className="dropdown-content">
+                      <a href="#">Cancel</a>
+                    </div>
+                  )}
+                </div>
               </td>
               <td>{subscription.emailAddress}</td>
             </tr>
