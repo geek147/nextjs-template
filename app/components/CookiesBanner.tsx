04be43cfe6ccd5c34cfe424972d44058d12e4ac6
Snowkids-code <oumaj47@gmail.com>
2023-10-24 05:06:46 +0300

    add pino logging

--- app/components/CookiesBanner.tsx
+++ app/components/CookiesBanner.tsx
@@ -1,9 +1,11 @@
 import CookieConsent from "react-cookie-consent";
 import Cookies from "js-cookie";
+import logger from "@/logger";
 
 const CookieBanner = () => {
   const handleAccept = () => {
     Cookies.set("Cookies", "accepted", { expires: 365 });
+    logger.info("cookies accepted")
     console.log("cookies accepted");
   };
   return (
